7247e78bf353390fc5e1941a08e770823d671869
Abhishek Gupta <abhishek.10004@gmail.com>
2016-02-10 00:18:27 +0530

    Created a folder for divide and conquer algorithm

---
+++ geeksforgeeks/algo/divide_and_conquer/find_element_appearing_once.cpp
@@ -0,0 +1,13 @@
+// http://www.geeksforgeeks.org/find-the-element-that-appears-once-in-a-sorted-array/
+
+#include <iostream>
+using namespace std;
+
+// 1st approach : take xor of all the elements and we will find the unique element
+
+int main()
+{
+	
+	
+	return 0;
+}
